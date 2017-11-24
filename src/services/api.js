6d90e6b14b1142123138ba8dd6fa92e0d504c111
Pogix3m <harold_x3m@yahoo.com>
2017-11-25 00:29:13 +0800

    Part 3

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,6 @@
 export function getRecentRecipes(app) {
   const recipes = app.service('recipes');
-  return recipes.find().then((data,err) => data.data);
+  return recipes.find({
+    query: {'$sort': { 'createdAt': -1}}
+  }).then((data,err) => data.data);
 }
